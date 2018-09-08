c6dc7f45b42ab8475a817fb272a620f22f065998
Ricardo Jean-Pierre <rjeanpierre90@gmail.com>
2018-09-08 19:45:00 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,12 @@
-def my_each # put argument(s) here
-  # code here
+def collection_select(array) 
+  i=0 
+  while
+   
+   yield |i|
+   
+   i += 1
+  end
+  
 end
+
+collection_select([1, 3, 4, 4,]) {|i| i + 2  }
